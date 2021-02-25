86e4b3a3b66c7b38794af696f048d9bbb4eb4fa3
Mark Carrington <MarkMpn@users.noreply.github.com>
2021-02-25 20:27:06 +0000

    Extension for case insensitive hash join

--- MarkMpn.Sql4Cds.Engine/ExecutionPlan/CaseInsensitiveObjectComparer.cs
+++ MarkMpn.Sql4Cds.Engine/ExecutionPlan/CaseInsensitiveObjectComparer.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MarkMpn.Sql4Cds.Engine.ExecutionPlan
 {
-    class CaseInsensitiveObjectComparer : IComparer<object>
+    class CaseInsensitiveObjectComparer : IComparer<object>, IEqualityComparer<object>
     {
         public int Compare(object x, object y)
         {
@@ -14,6 +14,16 @@ namespace MarkMpn.Sql4Cds.Engine.ExecutionPlan
                 return StringComparer.CurrentCultureIgnoreCase.Compare(s1, s2);
 
             return Comparer<object>.Default.Compare(x, y);
+        }
+
+        bool IEqualityComparer<object>.Equals(object x, object y)
+        {
+            return Compare(x, y) == 0;
+        }
+
+        public int GetHashCode(object obj)
+        {
+            return StringComparer.CurrentCultureIgnoreCase.GetHashCode(obj);
         }
 
         public static CaseInsensitiveObjectComparer Instance { get; } = new CaseInsensitiveObjectComparer();
