7901fc4becc1e291b12b13b63b713852614c194a
Mark Carrington <MarkMpn@users.noreply.github.com>
2021-03-19 21:41:17 +0000

    Added BaseDmlNode

---
+++ MarkMpn.Sql4Cds.Engine/ExecutionPlan/BaseDmlNode.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Metadata;
+
+namespace MarkMpn.Sql4Cds.Engine.ExecutionPlan
+{
+    public abstract class BaseDmlNode : BaseNode, IDmlQueryExecutionPlanNode
+    {
+        class ParallelConnectionSettings : IDisposable
+        {
+            private readonly int _connectionLimit;
+            private readonly int _threadPoolThreads;
+            private readonly int _iocpThreads;
+            private readonly bool _expect100Continue;
+            private readonly bool _useNagleAlgorithm;
+
+            public ParallelConnectionSettings()
+            {
+                _connectionLimit = System.Net.ServicePointManager.DefaultConnectionLimit;
+                ThreadPool.GetMinThreads(out _threadPoolThreads, out _iocpThreads);
+                _expect100Continue = System.Net.ServicePointManager.Expect100Continue;
+                _useNagleAlgorithm = System.Net.ServicePointManager.UseNagleAlgorithm;
+
+                System.Net.ServicePointManager.DefaultConnectionLimit = 65000;
+                ThreadPool.SetMinThreads(100, 100);
+                System.Net.ServicePointManager.Expect100Continue = false;
+                System.Net.ServicePointManager.UseNagleAlgorithm = false;
+            }
+
+            public void Dispose()
+            {
+                System.Net.ServicePointManager.DefaultConnectionLimit = _connectionLimit;
+                ThreadPool.SetMinThreads(_threadPoolThreads, _iocpThreads);
+                System.Net.ServicePointManager.Expect100Continue = _expect100Continue;
+                System.Net.ServicePointManager.UseNagleAlgorithm = _useNagleAlgorithm;
+            }
+        }
+
+        public string Sql { get; set; }
+
+        public int Index { get; set; }
+
+        public int Length { get; set; }
+
+        /// <summary>
+        /// Changes system settings to optimise for parallel connections
+        /// </summary>
+        /// <returns>An object to dispose of to reset the settings to their original values</returns>
+        protected IDisposable UseParallelConnections() => new ParallelConnectionSettings();
+
+        protected string GetDisplayName(int count, EntityMetadata meta)
+        {
+            if (count == 1)
+                return meta.DisplayName?.UserLocalizedLabel?.Label ?? meta.LogicalName;
+
+            return meta.DisplayCollectionName?.UserLocalizedLabel?.Label ??
+                meta.LogicalCollectionName ??
+                meta.LogicalName;
+        }
+
+        public abstract string Execute(IOrganizationService org, IAttributeMetadataCache metadata, IQueryExecutionOptions options, IDictionary<string, Type> parameterTypes, IDictionary<string, object> parameterValues);
+
+        public abstract IRootExecutionPlanNode FoldQuery(IAttributeMetadataCache metadata, IQueryExecutionOptions options, IDictionary<string, Type> parameterTypes);
+    }
+}
