a3bd10a9c0e924e9b86e31efa318a9c0acbf2e5f
Mark Carrington <MarkMpn@users.noreply.github.com>
2022-12-23 11:21:40 +0000

    Added signature help

--- MarkMpn.Sql4Cds.LanguageServer/Autocomplete/AutocompleteHandler.cs
+++ MarkMpn.Sql4Cds.LanguageServer/Autocomplete/AutocompleteHandler.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,6 +11,7 @@ using MarkMpn.Sql4Cds.LanguageServer.Admin;
 using MarkMpn.Sql4Cds.LanguageServer.Connection;
 using MarkMpn.Sql4Cds.LanguageServer.Workspace;
 using Microsoft.VisualStudio.LanguageServer.Protocol;
+using Microsoft.Xrm.Sdk;
 using StreamJsonRpc;
 using Range = Microsoft.VisualStudio.LanguageServer.Protocol.Range;
 
@@ -29,6 +32,7 @@ namespace MarkMpn.Sql4Cds.LanguageServer.Autocomplete
         {
             lsp.AddHandler(Methods.TextDocumentCompletion, HandleCompletion);
             lsp.AddHandler(Methods.TextDocumentHover, HandleHover);
+            lsp.AddHandler(Methods.TextDocumentSignatureHelp, HandleSignatureHelp);
         }
 
         public SumType<CompletionItem[], CompletionList>? HandleCompletion(CompletionParams request)
@@ -39,6 +43,9 @@ namespace MarkMpn.Sql4Cds.LanguageServer.Autocomplete
             var con = _con.GetConnection(request.TextDocument.Uri.ToString());
             var cons = _con.GetAllConnections();
             var acds = new Dictionary<string, AutocompleteDataSource>();
+
+            if (con == null)
+                return Array.Empty<CompletionItem>();
 
             foreach (var c in cons)
             {
@@ -80,6 +87,9 @@ namespace MarkMpn.Sql4Cds.LanguageServer.Autocomplete
             var cons = _con.GetAllConnections();
             var acds = new Dictionary<string, AutocompleteDataSource>();
 
+            if (con == null)
+                return null;
+
             foreach (var c in cons)
             {
                 EntityCache.TryGetEntities(c.Value.Connection, out var entities);
@@ -120,6 +130,136 @@ namespace MarkMpn.Sql4Cds.LanguageServer.Autocomplete
                     (SumType<string, MarkedString>)new MarkedString { Value = exactSuggestions[0].ToolTipText }
                 }
             };
+        }
+
+        private SignatureHelp HandleSignatureHelp(SignatureHelpParams request)
+        {
+            var doc = _doc.GetContent(request.TextDocument.Uri.ToString());
+            var lines = doc.Split('\n');
+            var pos = lines.Take(request.Position.Line).Sum(line => line.Length + 1) + request.Position.Character;
+
+            var depth = 1;
+            var paramIndex = 0;
+
+            for (var i = pos - 1; i >= 0; i--)
+            {
+                if (doc[i] == '(')
+                {
+                    depth--;
+
+                    if (depth == 0)
+                    {
+                        // Find method name
+                        var wordLength = new Regex("\\b").Match(new string(doc.Substring(0, i).Reverse().ToArray()), 1).Index;
+                        var wordStart = i - wordLength;
+                        var functionName = doc.Substring(wordStart, wordLength);
+                        var con = _con.GetConnection(request.TextDocument.Uri.ToString());
+
+                        if (con == null)
+                            return null;
+
+                        if (con.DataSource.MessageCache.TryGetValue(functionName, out var message) &&
+                            message.IsValidAsTableValuedFunction())
+                        {
+                            var availableParameters = message.InputParameters
+                                .OrderBy(p => p.Position)
+                                .Select(p => new ParameterInformation
+                                {
+                                    Label = p.Name,
+                                    Documentation = p.Name + " " + CLRToSQLTypeName(p.Type)
+                                })
+                                .ToArray();
+
+                            return new SignatureHelp
+                            {
+                                ActiveSignature = 0,
+                                ActiveParameter = paramIndex,
+                                Signatures = new[]
+                                {
+                                    new SignatureInformation
+                                    {
+                                        Label = message.Name,
+                                        Documentation = "Dataverse Message",
+                                        Parameters = availableParameters
+                                    }
+                                }
+                            };
+                        }
+                        else
+                        {
+                            var function = typeof(FunctionMetadata.SqlFunctions)
+                                .GetMethods(BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.Public)
+                                .FirstOrDefault(m => m.Name.Equals(functionName, StringComparison.OrdinalIgnoreCase));
+
+                            if (function != null)
+                            {
+                                var availableParameters = function
+                                    .GetParameters()
+                                    .Select(p => new ParameterInformation
+                                    {
+                                        Label = p.Name,
+                                        Documentation = (p.Name + " " + CLRToSQLTypeName(p.ParameterType)).Trim()
+                                    })
+                                    .ToArray();
+
+                                return new SignatureHelp
+                                {
+                                    ActiveSignature = 0,
+                                    ActiveParameter = paramIndex,
+                                    Signatures = new[]
+                                    {
+                                        new SignatureInformation
+                                        {
+                                            Label = CLRToSQLTypeName(function.ReturnType) + " " + function.Name.ToUpperInvariant() + "(" + String.Join(", ", availableParameters.Select(p => (string)p.Documentation)) + ")",
+                                            Documentation = function.GetCustomAttribute<DescriptionAttribute>()?.Description ?? "SQL Function",
+                                            Parameters = availableParameters
+                                        }
+                                    }
+                                };
+                            }
+                        }
+                    }
+                }
+                else if (doc[i] == ')')
+                {
+                    depth++;
+                }
+                else if (doc[i] == ',' && depth == 1)
+                {
+                    paramIndex++;
+                }
+            }
+
+            return null;
+        }
+
+        private string CLRToSQLTypeName(Type type)
+        {
+            if (type == typeof(string))
+                return "nvarchar";
+
+            if (type == typeof(int))
+                return "int";
+
+            if (type == typeof(long))
+                return "bigint";
+
+            if (type == typeof(DateTime))
+                return "datetime";
+
+            if (type == typeof(Guid))
+                return "uniqueidentifier";
+
+            if (type == typeof(EntityReference))
+                return "[MarkMpn.Sql4Cds.Engine.SqlEntityReference]";
+
+            if (type == typeof(double))
+                return "double";
+
+            if (type == typeof(object))
+                return "";
+
+            return type.Name.ToLowerInvariant();
         }
     }
 }
