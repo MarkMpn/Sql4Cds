e1023c7abb8c79e0352deb59a6c2e2da1160f6a7
Mark Carrington <MarkMpn@users.noreply.github.com>
2023-01-16 22:13:25 +0000

    Added autocomplete for optionset values

--- MarkMpn.Sql4Cds.Tests/AutocompleteTests.cs
+++ MarkMpn.Sql4Cds.Tests/AutocompleteTests.cs
@@ -335,5 +335,25 @@ namespace MarkMpn.Sql4Cds.Tests
 
             CollectionAssert.AreEqual(Array.Empty<string>(), suggestions);
         }
+
+        [TestMethod]
+        public void SuggestOptionSetValuesWhere()
+        {
+            var sql = "SELECT * FROM new_customentity WHERE new_optionsetvalue = ";
+
+            var suggestions = _autocomplete.GetSuggestions(sql, sql.Length - 1).Select(s => s.Text).ToList();
+
+            CollectionAssert.IsSubsetOf(new[] { "100001", "100002", "100003" }, suggestions);
+        }
+
+        [TestMethod]
+        public void SuggestOptionSetValuesSet()
+        {
+            var sql = "UPDATE new_customentity SET new_optionsetvalue = ";
+
+            var suggestions = _autocomplete.GetSuggestions(sql, sql.Length - 1).Select(s => s.Text).ToList();
+
+            CollectionAssert.IsSubsetOf(new[] { "100001", "100002", "100003" }, suggestions);
+        }
     }
 }
