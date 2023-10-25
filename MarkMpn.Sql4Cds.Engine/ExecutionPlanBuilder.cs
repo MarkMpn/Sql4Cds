9ae6aa7a41cd2fa6f415c146a2d8b533085c22e4
Mark Carrington <MarkMpn@users.noreply.github.com>
2023-10-25 14:42:03 +0100

    Allow using columns from outer queries in joins Fixes #376

--- MarkMpn.Sql4Cds.Engine/ExecutionPlanBuilder.cs
+++ MarkMpn.Sql4Cds.Engine/ExecutionPlanBuilder.cs
@@ -3840,6 +3840,9 @@ namespace MarkMpn.Sql4Cds.Engine
                 var rhsSchema = rhs.GetSchema(context);
                 var fixedValueColumns = GetFixedValueColumnsFromWhereClause(query, lhsSchema, rhsSchema);
 
+                // Capture any references to data from an outer query
+                CaptureOuterReferences(outerSchema, null, join.SearchCondition, context, outerReferences);
+
                 var joinConditionVisitor = new JoinConditionVisitor(lhsSchema, rhsSchema, fixedValueColumns);
                 join.SearchCondition.Accept(joinConditionVisitor);
 
