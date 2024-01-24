032bac51536984bd7b489b6b47c6644fdd91a4aa
Mark Carrington <31017244+MarkMpn@users.noreply.github.com>
2024-01-24 22:08:11 +0000

    Improved reporting of constraint errors

--- MarkMpn.Sql4Cds.XTB/SqlQueryControl.cs
+++ MarkMpn.Sql4Cds.XTB/SqlQueryControl.cs
@@ -1039,7 +1039,7 @@ namespace MarkMpn.Sql4Cds.XTB
                                 constraintError = true;
                                 foreach (DataRow row in dataTable.Rows)
                                 {
-                                    if (row.RowError != null)
+                                    if (row.HasErrors && row.RowError != null)
                                         throw new ConstraintException(row.RowError, ex);
                                 }
 
